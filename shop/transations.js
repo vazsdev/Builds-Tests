534fcc5726539dded111eb895a08804ca05e75be
Vinicius de Azevedo Silveira <vazsdev@gmail.com>
2026-03-04 10:17:09 -0300

    Update transations.js

--- shop/transations.js
+++ shop/transations.js
@@ -8,9 +8,10 @@ const transactions = [
   { id: 5, user: "Ana", type: "compra", value: 200, date: "2024-02" },
   { id: 6, user: "Carla", type: "compra", value: 500, date: "2024-02" },
   { id: 7, user: "Carla", type: "estorno", value: 500, date: "2024-03" },
-  { id: 8, user: "Bruno", type: "compra", value: 250, date: "2024-03" },
+  { id: 8, user: "Bruno", type: "compra", value: 1000, date: "2024-03" },
 ];
 
+//Serviço de Transações
 const transactionsService = {
   getValidTransactions(transactions) {
     return transactions.reduce((validTransaction, item) => {
@@ -32,11 +33,55 @@ const transactionsService = {
     }, {});
   },
 
+  getTotalRevenue(transactions) {
+    const total = this.getValues(transactions);
+
+    return total.reduce((total, value) => {
+      return total + value;
+    }, 0);
+  },
+
+  getBiggestPurchase(transactions) {
+    const purchases = this.getValidTransactions(transactions);
+
+    return Object.values(purchases).reduce((total, item) => {
+      if (total === null) {
+        return item;
+      }
+      if (item.value > total.value) {
+        return item;
+      }
+      return total;
+    }, null);
+  },
+
+  getPurchasesMedia(transactions) {
+    const values = this.getValues(transactions);
+
+    const total = values.reduce((total, value) => {
+      return total + value;
+    }, 0);
+
+    return total / values.length;
+  },
+
   generate(transactions) {
     return {
       validTransactions: this.getValidTransactions(transactions),
       onlyValues: this.getValues(transactions),
       chargebacks: this.getChargebacks(transactions),
+      total: this.getTotalRevenue(transactions).toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL",
+      }),
+      biggestPurchase: this.getBiggestPurchase(transactions),
+      purchasesMedia: this.getPurchasesMedia(transactions).toLocaleString(
+        "pt-br",
+        {
+          style: "currency",
+          currency: "BRL",
+        },
+      ),
     };
   },
 };
